51f5e3b96e8f97f418b5f4e566b2b34b802f9e66
reg734 <33149497+reg734@users.noreply.github.com>
2025-07-07 15:52:16 +0800

    完成汽車瀏覽頁面

--- just-rent-sample/app/controllers/__init__.py
+++ just-rent-sample/app/controllers/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+
 bp = Blueprint('controller', __name__)
 
 from app.controllers import pages_controller
--- just-rent-sample/app/controllers/admin_controller.py
+++ just-rent-sample/app/controllers/admin_controller.py
@@ -1,8 +1,13 @@
 from app.controllers import bp
 from flask import render_template
-
+from app import db
+from sqlalchemy import text
 
 @bp.route('/admin')
 def admin_index():
-    return render_template('admin/index.html')
+    sql = text("SELECT * FROM cars")
+    result = db.session.execute(sql)
+    cars = [row for row in result]
+    return render_template('admin/index.html', cars=cars)
+
 
