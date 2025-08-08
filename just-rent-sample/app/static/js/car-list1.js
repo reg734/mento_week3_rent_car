8cef7ca2f4a4fb21b55a77e680ac455d4045a248
reg734 <33149497+reg734@users.noreply.github.com>
2025-08-08 14:34:47 +0800

    串好car_list1

---
+++ just-rent-sample/app/static/js/car-list1.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $.ajax({
+        url: `/api/cars`,
+        method: 'GET',
+        success: function (data) {
+            const carListContainer = $('#car-list .row');
+            carListContainer.empty();
+
+            data.forEach(function(car) {
+                const carHtml = `
+                <div class="col-xl-4 col-lg-6">
+                    <div class="de-item mb30">
+                        <div class="d-img">
+                            <img src="${car.image_url || '/static/images/cars/jeep-renegade.jpg'}" class="img-fluid" alt="">
+                        </div>
+                        <div class="d-info">
+                            <div class="d-text">
+                                <h4>${car.name}</h4>
+                                <div class="d-item_like">
+                                    <i class="fa fa-heart"></i><span>25</span>
+                                </div>
+                                <div class="d-atr-group">
+                                    <span class="d-atr"><img src="/static/images/icons/1.svg" alt="">${car.seats}</span>
+                                    <span class="d-atr"><img src="/static/images/icons/2.svg" alt="">${car.luggage_capacity}</span>
+                                    <span class="d-atr"><img src="/static/images/icons/3.svg" alt="">${car.doors}</span>
+                                    <span class="d-atr"><img src="/static/images/icons/4.svg" alt="">${car.body}</span>
+                                </div>
+                                <div class="d-price">
+                                    Daily rate from <span>$${car.daily_rate}</span>
+                                    <a class="btn-main" href="/cars/${car.id}">Rent Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+                carListContainer.append(carHtml);
+            });
+        },
+        error: function () {
+            $('#car-list .row').html('<p>載入車輛資料失敗</p>');
+        }
+    });
+});
